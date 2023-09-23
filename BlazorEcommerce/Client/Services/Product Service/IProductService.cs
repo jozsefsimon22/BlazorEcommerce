de443595a343c3f567292d9ef187f65c8b0b63db
Jozsef Simon <nex.simon@gmail.com>
2023-09-23 09:34:37 +0100

    Added gitignore file

---
+++ BlazorEcommerce/Client/Services/Product Service/IProductService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEcommerce.Client.Services.Product_Service
+{
+    public interface IProductService
+    {
+        List<Product> Products { get; set; }
+        Task GetProducts();
+        Task<ServiceResponse<Product>> GetProduct(int productId);
+    }
+}
--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -1,14 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorEcommerce.Shared
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
+
         [Column(TypeName = "decimal(18,4)")]
         public decimal Price { get; set; }
+
+        public Category? Category { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
     }
 }
